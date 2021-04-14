f331e951128fa4d700e1ba270e3a70859682adb0
Robert Zhang <louirobert@gmail.com>
2021-04-14 13:26:30 +0800

    Init commit.

---
+++ AppPackage/Hello.ps1
@@ -0,0 +1,5 @@
+param(
+    [string] $name = "Batch"
+)
+
+"Hello, $name!"
---
+++ ListNodeAgentSkus/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Azure.Batch;
+using Microsoft.Azure.Batch.Auth;
+using System;
+
+namespace ListNodeAgentSkus
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // Batch account credentials
+            var BatchAccountName = Environment.GetEnvironmentVariable("BatchAccountName");
+            var BatchAccountKey = Environment.GetEnvironmentVariable("BatchAccountKey");
+            var BatchAccountUrl = Environment.GetEnvironmentVariable("BatchAccountUrl");
+
+            if (String.IsNullOrEmpty(BatchAccountName) ||
+                String.IsNullOrEmpty(BatchAccountKey) ||
+                String.IsNullOrEmpty(BatchAccountUrl))
+            {
+                throw new InvalidOperationException("One or more Batch credentials are not specified.");
+            }
+
+            // Get a Batch client using account creds
+
+            BatchSharedKeyCredentials cred = new BatchSharedKeyCredentials(BatchAccountUrl, BatchAccountName, BatchAccountKey);
+
+            using (BatchClient batchClient = BatchClient.Open(cred))
+            {
+                var skus = batchClient.PoolOperations.ListNodeAgentSkus();
+                skus.ForEachAsync(sku =>
+                {
+                    var format = @"
+Image Publisher = {0}
+Image Offer = {1}
+Image Sku = {2}
+Image Version = {3}
+";
+                    Console.WriteLine($"## Node Agent SKU: {sku.Id}");
+                    foreach (var img in sku.VerifiedImageReferences)
+                    {
+                        Console.WriteLine(String.Format(format, img.Publisher, img.Offer, img.Sku, img.Version));
+                    }
+                }).GetAwaiter().GetResult();
+
+            }
+        }
+    }
+}
